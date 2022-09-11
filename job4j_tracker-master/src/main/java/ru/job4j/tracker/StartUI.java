9f40e12a43e5ec456d67f1fc4d21816c31ef6b32
Honcharikvv <goncharikvv@gmail.com>
2022-09-11 22:30:48 +0100

    7. Date. Отображение даты. [#310017]

---
+++ job4j_tracker-master/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime created = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatterCreated = created.format(formatter);
+        System.out.println(formatterCreated);
+    }
+}
